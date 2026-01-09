33b87731f907badb95f5ed16beb1f059285b8a37
ayemintun <amtamt547@gmail.com>
2026-01-09 22:31:56 +0630

    update cv and fix design error on projects

--- components/projects/ProjectDetail.jsx
+++ components/projects/ProjectDetail.jsx
@@ -3,7 +3,7 @@ import ButtonSection from "./ButtonSection";
 const ProjectDetail = ({ selectedProject }) => {
   return (
     <div className="md:w-2/3 w-full md:h-[65vh] h-[55vh] overflow-y-scroll md:px-7 py-0">
-      <h1 className="dark:text-white font-bold md:text-3xl text-xl mb-4 md:mt-0 mt-4 sticky top-0 py-3">
+      <h1 className="dark:text-white font-bold md:text-3xl text-xl mb-4 md:mt-0 mt-4 sticky top-0 py-3 bg-gray-800">
         {selectedProject.name}
       </h1>
       <p className="dark:text-white text-pretty md:text-lg text-base mb-3">
